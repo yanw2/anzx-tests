0803627d8ac8ab8ebc0d8256be43d7a3436e299a
Wenjing Yan <wenjingyn@gmail.com>
2021-02-01 16:59:14 +1100

    Get server.py working

---
+++ test1-q2/api-server/server.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, jsonify, make_response
+app = Flask(__name__)
+
+@app.route('/status', methods=['GET'])
+def getStatus():
+    info = {
+        'version': os.getenv('VERSION'),
+        'lastcommitsha': os.getenv('LAST_COMMIT_SHA'),
+        'description': 'pre-interview technical test'
+    }
+
+    return jsonify({'myapplication': info})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.getenv('PORT'), debug=True)
