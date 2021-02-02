6af2e316cd3ecb02dc92ef9989b2c00faebe935b
Wenjing Yan <wenjingyn@gmail.com>
2021-02-02 15:50:36 +1100

    Update test2-q1 solution

--- test2-q1/main.go
+++ test2-q1/main.go
@@ -19,8 +19,9 @@ func main() {
 	r.HandleFunc("/opt", opttruth)
 
 	s := &http.Server{
-		Handler:      r,
-		Addr:         "127.0.0.1:8000",
+		Handler: r,
+		Addr:    "0.0.0.0:8000",
+		//Addr:         "127.0.0.1:8000",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
